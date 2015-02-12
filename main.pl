b58bb4762a855e54d2155ff2abada3fd469dc5df
Helene Tindon <helene.tindon@wanadoo.fr>
2015-02-12 17:55:12 +0100

    ask and correct the question

--- main.pl
+++ main.pl
@@ -1,7 +1,9 @@
 use strict;
 use warnings;
 use Data::Dumper;
+use List::Util qw/shuffle/;
 
+# STEP 0: The user is asked which file contains the data for the quizz session
 #print "Which quizz file do you want to use?\n";
 #my $filename = <>;
 #chomp($filename);
@@ -9,18 +11,23 @@ use Data::Dumper;
 #open (my $fh, '<:encoding(UTF-8)', $filename)
 #or die "Could not open file '$filename' $!";
 
+# STEP 1: The data from the file is extracted and put into a dictionnary
+
+# STEP 2: A number X of questions are asked and immediately corrected
+
 my %hash = ();
 
 $hash{ 'key1' } = ['value1.1', 'value2.1'];
 $hash{ 'key2' } = ['value1.2'];
 $hash{ 'key3' } = ['value1.3', 'value2.3', 'value3.3'];
 
-# I will store in an array all of the keys contained in my dictionnary
-#my @listOfQuestions = keys %hash;
-#my $question = $listOfQuestions[0]; #it's random anyway because of the hash
-
-
 ask(\%hash);
+
+# STEP 3: A total score is given and the session ends
+
+#close $fh;
+
+##########################################
 
 sub ask {
 # This subroutine will display a question and all the possible answers
@@ -42,15 +49,26 @@ sub ask {
 # The elements of an hash are not sorted so no randomness is needed.
 	my $question = $listOfQuestions[0];
 	my @answers = @{ $hash{ $question } };
+# The elements of a list are sorted so I will need to add some randomness
+# But first I store the right answer which is always the first one
+	my $rightAnswer = $answers[0];
+	@answers = shuffle(@answers);
 	print "Question: $question\n";
 	my $letter = 'A';
 	for my $i (0 .. $#answers) {
-		print "$letter $answers[$i]\n";
+		if ($answers[$i] eq $rightAnswer) {
+			$rightAnswer = $letter;
+		}
+		$answers[$i] = $letter . ' ' . $answers[$i];
+		print "$answers[$i]\n";
 		$letter++;
 	}
 	print "Your Answer: ";
 	my $userAnswer = <STDIN>;
 	chomp $userAnswer;
-} 
-
-#close $fh;
+	if ($userAnswer eq $rightAnswer) {
+		print "True\n";
+	} else {
+		print "False\n"
+	}
+}
